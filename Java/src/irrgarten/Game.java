ad58e11ecdb7ee91a723b4e79dbd6e7c05d7da4f
Álvaro Maldonado Medina <126078785+elmandelbetis@users.noreply.github.com>
2023-10-14 18:46:40 +0200

    Añadida clase Game

---
+++ Java/src/irrgarten/Game.java
@@ -0,0 +1,101 @@
+///////////////////////////////////////
+// Álvaro Maldonado Medina     	     //
+// 2D - D3                           //
+// Fichero: irrgarten/Game.java      //
+///////////////////////////////////////
+
+
+package irrgarten;
+
+
+public class Game {
+    
+    private static int MAX_ROUNDS = 10;
+    private int currentPlayerIndex;
+    private String log;
+    
+    
+    public int Game(int nplayers) // ????
+    {
+        this.currentPlayerIndex = nplayers;
+    }
+    
+    public boolean finished()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    public boolean nexStep(Directions preferredDirection)
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    public GameState getGameState()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    public void configureLabyrinth()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    private void actualDirection(Directions preferredDirection)
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    private GameCharacter combat(Monster monster)
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    private void manageReward(GameCharacter winner)
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    private void manageResurrection()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    private void logPlayerWon()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    private void logMonsterWon()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    private void Resurrected()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    private void PlayerSkipTurn()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    private void logPlayerNoOrders()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    private void logNoMonster()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    private void logRounds()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    
+    
+    
+}
